a604e8c7f9787c1f1e4fb57305b986371edb5c02
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-06-19 21:23:46 -0500

    Changed the functions to promised functions

---
+++ index2.js
@@ -0,0 +1,14 @@
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    console.log(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
